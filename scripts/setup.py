9621274d3f961341485687119d768e03411b1cdf
Aaron Teo <aaron.teo@riv-alumni.com>
2022-10-23 17:23:58 +0800

    feat(scripts): add setup

---
+++ scripts/setup.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import subprocess
+
+__author__ = 'Aaron Teo'
+__copyright__ = 'Copyright 2022, IT1566 App Dev Project'
+__license__ = 'MIT'
+
+def setup():
+  print('Upgrading pip...')
+  subprocess.call('python3 -m pip install --upgrade pip'.split(' '), stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
+
+  if not os.path.isdir('venv'):
+    print(f'VirtualEnv not found. Creating now...')
+    subprocess.call('py -m pip install venv'.split(' '), stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
+    subprocess.call('py -m venv venv'.split(' '), stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
+  
+  print('Installing packages from requirements.txt...')
+  subprocess.call('python3 -m pip install -r requirements.txt'.split(' '), stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
+
+  print('All done.')
+
+if __name__ == '__main__':
+  setup()
