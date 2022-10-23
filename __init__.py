92faf950803e71e1fa623ac1d26295ad78f81def
Aaron Teo <aaron.teo@riv-alumni.com>
2022-10-24 04:20:33 +0800

    refactor: render index.html in flask

--- __init__.py
+++ __init__.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from sys import flags
+from flask import Flask, render_template
+
+DEV_MODE = flags.dev_mode
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def root():
-  return 'Hello World'
+  return render_template('index.html')
 
 if __name__ == '__main__':
-  app.run()
+  app.run(host='0.0.0.0', port=3000, debug=DEV_MODE)
