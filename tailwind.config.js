8afcb82c6a9381bc777c92cd4fdca257340daa75
Aaron Teo <aaron.teo@riv-alumni.com>
2022-10-24 04:20:48 +0800

    feat: add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.html',
+    './static/**/*.{js,css}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
