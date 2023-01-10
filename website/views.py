d5cd324591d6aae4182e25447a64c3a63ece4ec8
Travis Teng <kaezokurasaki@gmail.com>
2023-01-11 00:27:39 +0800

    fixed the spacing in routing on line 46

--- website/views.py
+++ website/views.py
@@ -13,36 +13,46 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 @views.route('/home')
+
 def home():
   return render_template('home.html')
 @views.route('/wallet')
 def wallet():
   return render_template("wallet.html")
+
 @views.route('/retrieveacc')
 @login_required
 def retreieveacc():
   return render_template("retrieveacc.html")
+
 @views.route('/signup')
 def signup():
   return render_template("signup.html")
+
 @views.route('/login')
 def login():
   return render_template("login.html")
+
 @views.route('/carlistings')
 def carlistings():
   return render_template("car listings.html")
+
 @views.route('/contractorlistings')
 def contractorlistings():
   return render_template('contractor listings.html')
+
 @views.route('/createcarlistings')
 def createcarlistings():
   return render_template('create car listings.html')
+
 @views.route('createcontractorlistings')
 def createcontractorlistings():
   return render_template('create contractor listings.html')
+
 @views.route('/updatecarlistings')
 def updatecarlistings():
   return render_template('update car listings.html')
-@views.route('/update contractor listings')
+
+@views.route('/updatecontractorlistings')
 def updatecontractorlistings():
   return render_template('update contractor listings.html')
