6bbc5a5fb8732a71518d0e1c02b0a6b84e0837a3
Dexter Teo <dexter.teo1320@gmail.com>
2023-01-12 04:59:42 +0800

    feat: finish crud for vehiclestore

--- website/views.py
+++ website/views.py
@@ -36,12 +36,12 @@ def vehiclestore():
         vehicles.append(db[i])
   return render_template("VehicleStore.html", vehicles = vehicles)
 
-@views.route('/UpdateVehicle',methods = ['GET','POST','PUT'])
+@views.route('/UpdateVehicle',methods = ['GET','POST'])
 @login_required
 def updatevehicle():
-  with shelve.open(DB_USER_LOCATION) as db:
-  owner_uid = 
-  return render_template("UpdateVehicle.html")
+  vehicles = []
+  with shelve.open(DB_VEHICLE_LOCATION) as db:
+    return render_template("UpdateVehicle.html", owner_uid = current_user.email)
 
 @views.route('/retrieveacc')
 @login_required
@@ -93,10 +93,3 @@ def updateA():
   with shelve.open(DB_USER_LOCATION) as db:
     return render_template("UpdateAcc.html", user=db[email])
 
-@views.route('/VehicleStore')
-def VehicleStore():
-  return render_template("VehicleStore.html")
-
-@views.route('/UpdateVehicle')
-def updateV():
-  return render_template("UpdateVehicle.html")
